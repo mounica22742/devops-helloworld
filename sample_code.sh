c2dc8714ee5229c060461e5b1b740bacb67db91c
mounica22742 <121622974+mounica22742@users.noreply.github.com>
2023-01-04 23:06:21 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,7 @@
 
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
-echo "This is a sample"
+echo "This is a sample1"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
